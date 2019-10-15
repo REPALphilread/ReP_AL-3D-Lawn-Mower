b8928e056b137a71bc6251f041bc81a4f7929bcb
REPALphilread <47027434+REPALphilread@users.noreply.github.com>
2019-10-15 14:37:50 +0200

    Update WIFI.ino

--- Mower Arduino Code/Mower_V6.7/MEGA_V6.7/WIFI.ino
+++ Mower Arduino Code/Mower_V6.7/MEGA_V6.7/WIFI.ino
@@ -143,12 +143,12 @@ delay(30);
    val_WIFI = 0;   // restes val2 to zero so the command is only executed once
   }
 
-// Manuel Button in Blynk App
+// Manual Button in Blynk App
  if (val_WIFI == 15)  {    
    Serial.println("");
    Serial.print(F("WIFI Command: ")); 
    Serial.print(val_WIFI);
-   Serial.println(F("|Manuel Mode"));
+   Serial.println(F("|Manual Mode"));
    Manouver_Park_The_Mower(); 
    delay(1000);
    Manouver_Manuel_Mode(); 
