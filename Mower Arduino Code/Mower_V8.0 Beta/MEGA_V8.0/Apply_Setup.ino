2d33289725e7aab6a4a4911d550eecb26364d1c0
REPALphilread <47027434+REPALphilread@users.noreply.github.com>
2020-05-04 15:51:56 +0200

    Add files via upload

---
+++ Mower Arduino Code/Mower_V8.0 Beta/MEGA_V8.0/Apply_Setup.ino
@@ -0,0 +1,181 @@
+void Print_Mower_Status() {
+  //Serial.print("Mower Stat....");
+  if (Mower_Docked == 1) Serial.print(F("Docked:1|"));
+  if (Mower_Parked == 1) Serial.print(F("Parked:1|"));
+  if (Mower_Running == 1) Serial.print(F("Running:1|"));
+  if (Manuel_Mode == 1) Serial.print(F("Manuel Mode:1|"));
+  if (Mower_Parked_Low_Batt == 1) Serial.print(F("Park_Low_Batt:1|"));
+  if (Mower_Error == 1) Serial.print(F("Mower Error:1|"));
+  
+}
+
+
+void Setup_Tilt_Tip_Safety() {
+  if (Angle_Sensor_Enabled == 1)     pinMode(Tilt_Angle, INPUT);//define Data input pin input pin
+  if (Tip_Over_Sensor_Enabled == 1)  pinMode(Tilt_Orientation, INPUT);//define Data input pin input
+  }
+
+void Prepare_Mower_from_Settings() {
+
+  if (Use_Charging_Station == 1) {
+     Mower_Docked  = 1;
+     Mower_Parked  = 0;
+     Mower_Running = 0;
+     }
+
+
+if (Use_Charging_Station == 0) {
+    Mower_Docked  = 0;
+    Mower_Parked  = 1;
+    Mower_Running = 0;
+    }
+ }
+
+
+void Setup_Compass() {
+ 
+  if (Compass_Activate == 1) {
+
+    
+  /*Setup Compass
+  *************************************************************************/
+  lcd.clear();
+  lcd.print(F("Compass  "));
+  lcd.setCursor(0, 1);
+  lcd.print(F("Setup"));
+
+  int Compass_Attempt = 0;
+  int Compass_Found = 0;
+
+      if (Compass_Type == 1) {
+        //HMC5883 Compass
+        while ((!compass.begin()) && (Compass_Attempt <= 5)){
+          Serial.println(F("Could not find a valid QMC5883 sensor, check wiring!"));
+          delay(500);
+          Compass_Attempt = Compass_Attempt + 1;
+          }
+        if (compass.isHMC()) {
+          Serial.println(F("Initialize DF Robot HMC5883 Compass"));
+          lcd.setCursor(6,0);
+          lcd.print(F(": HMC5883"));
+          delay(500);
+          compass.setRange(HMC5883L_RANGE_1_3GA);
+          compass.setMeasurementMode(HMC5883L_CONTINOUS);
+          compass.setDataRate(HMC5883L_DATARATE_15HZ);
+          compass.setSamples(HMC5883L_SAMPLES_8);
+          Compass_Found = 1;
+          }
+       //QMC5883 Compass
+        else if (compass.isQMC()) {
+            Serial.println(F("Initialising DF Robot QMC5883 Compass"));
+            lcd.setCursor(6,0);
+            lcd.print(F(": QMC5883"));
+            delay(500);
+            compass.setRange(QMC5883_RANGE_2GA);
+            compass.setMeasurementMode(QMC5883_CONTINOUS); 
+            compass.setDataRate(QMC5883_DATARATE_50HZ);
+            compass.setSamples(QMC5883_SAMPLES_8);
+            Compass_Found = 1;
+            }
+      
+    
+      if ((Compass_Attempt > 5) && (Compass_Found == 0)) {
+        Serial.println("No Valid Compass Found");
+        Compass_Activate = 0;
+        Serial.println("Compass Deactivated");
+        delay(3000);
+      }
+      lcd.setCursor(0,1); 
+      lcd.print(F("Done!             "));
+      delay(500);
+      lcd.clear();
+    }
+  
+  if (Compass_Type == 2) {
+        Serial.println("Compass Selected GY-521");
+        Wire.begin();
+        Wire.beginTransmission(MPU_addr);
+        Wire.write(0x6B);
+        Wire.write(0);
+        Wire.endTransmission(true);  
+  }
+
+  if (Compass_Activate == 0) {
+      Serial.println(F("Compass Switched off - Select 1 in setup to switch on.")); 
+      }
+ }
+}
+
+
+void Setup_Relays() {
+  Serial.println(F("Setup Relays"));
+  pinMode(Relay_Motors, OUTPUT);
+  delay(5);
+  Turn_Off_Relay();
+  delay(5);
+  }
+
+void Setup_Motor_Pins() {
+  Serial.println(F("Setup Motor Pins"));
+  pinMode(L_EN, OUTPUT);
+  pinMode(R_EN, OUTPUT);
+  pinMode(RPWM, OUTPUT);
+  }
+
+
+void  Turn_On_Relay() {
+   Serial.print(F("Relay:ON|"));
+   digitalWrite(Relay_Motors, LOW);                         // Turn of the relay for the main battery power
+   }
+
+void  Turn_Off_Relay() {
+   Serial.print(F("Relay:Off|"));
+   digitalWrite(Relay_Motors, HIGH);                         // Turn of the relay for the main battery power
+   }
+
+void Setup_Membrane_Buttons() {
+  Serial.println(F("Setup Membrane Keys"));
+  pinMode(Start_Key, INPUT_PULLUP);            // set pin as input
+  pinMode(Plus_Key, INPUT_PULLUP);            // set pin as input
+  pinMode(Minus_Key, INPUT_PULLUP);            // set pin as input
+  pinMode(Stop_Key, INPUT_PULLUP);            // set pin as input  
+  }
+
+
+void Setup_Bumper_Bar() {
+  pinMode(Bumper_Switch_Frnt_RH, INPUT_PULLUP); 
+  pinMode(Bumper_Switch_Frnt_LH, INPUT_PULLUP); 
+}
+
+void Setup_ADCMan() {
+  Serial.println(F("ADCMAN"));
+  ADCMan.init();
+  perimeter.setPins(pinPerimeterLeft, pinPerimeterRight);
+  perimeter.useDifferentialPerimeterSignal = true;
+  perimeter.speedTest();
+  ADCMan.run();
+}
+
+
+void Setup_Check_Pattern_Mow() {
+  if (Pattern_Mow == 1) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Pattern Mow");
+    lcd.setCursor(0,1);
+    lcd.print("Parallel");
+    delay(1000);
+    lcd.clear();
+    }
+  if (Pattern_Mow == 2) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Pattern");
+    lcd.setCursor(0,1);
+    lcd.print("Spirals");
+    delay(1000);
+    lcd.clear();
+    }
+}
+    
+  
---
+++ Mower Arduino Code/Mower_V8.0 Beta/Readme.txt
@@ -0,0 +1,42 @@
+ReP_AL 3D Printed Robot Lawn Mower
+----------------------------------
+
+Code V8.0  BETA    04.05.2020
+------------------------------
+
+Upload the MEGA code to the Arduino MEGA
+Upload the Nano code to the Arduino Nano
+Upload the NodeMCU code to the NodeMCU Board   (includes new code)
+Upload the TFT Code to the TFT MEGA Shield
+Upload the BMP Files to the TFT SD Card
+
+
+Features of 8.0 BETA
+--------------------
+
+- TFT Display is added (TFT_Screen_Menu = 1 in Settings to turn on TFT function
+			Ensure LCD_Scree_Keypad_Menu = 0 )
+- Bumper Bar is activated in finding wire function
+- Angle Stop Capability added
+- Tip Over Safety Stop Capability added
+
+Experimental
+- Slow at wire function - Slows the mower when it gets to the wire to ensure the wire is detected in time.
+
+
+Known Issues of 8.0 BETA
+------------------------
+- With TFT longer reaction time than normal to detect wire (see slow at wire function to help)
+- TFT Some menus get stuck when returning to the home screen
+- TFT Test scripts dont always start correctly
+
+
+
+NodeMCU Board URL:
+------------------
+Use this URL to add the NodeMCU board 1.0 to the boards menu.
+
+http://arduino.esp8266.com/stable/package_esp8266com_index.json
+
+
+ 
